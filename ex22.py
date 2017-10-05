e90b16c5285e1cfb6b5ed4019c4ab91b2c422438
rooliveiraf <32530942+rooliveiraf@users.noreply.github.com>
2017-10-05 00:24:08 -0300

    Delete ex22.py

--- ex22.py
+++
@@ -1,6 +0,0 @@
-Python 3.5.2 (default, Sep 14 2017, 22:51:06) 
-[GCC 5.4.0 20160609] on linux
-Type "copyright", "credits" or "license()" for more information.
->>> 10 % 3 *10 ** 2 + 1 -10 * 4 /2
-81.0
->>> 
